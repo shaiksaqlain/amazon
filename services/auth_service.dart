00be32e4d2597f01e05610023a66f549ccb61ad0
SHAIK SAQLAIN <shaiksaqlainlucifer@gmail.com>
2022-08-25 12:02:21 +0530

    SignUp API 💡

---
+++ services/auth_service.dart
@@ -0,0 +1,15 @@
+class AuthService {
+  //sign up user
+
+  void signUpUser({
+    required String email,
+    required String password,
+    required String name,
+  })async {
+try {
+  
+} catch (e) {
+  
+}
+  }
+}
